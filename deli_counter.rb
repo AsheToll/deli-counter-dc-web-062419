659521c24fd425544dfb396b8e91986752467739
Ashley Tolen <tolenashley7@gmail.com>
2019-05-16 16:05:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 def line(deli)
   if deli.length > 0
     deli.each do |name|
-      puts "The line is currently: #{deli.[i]}. #{name}"
+      puts "The line is currently: #{deli[i]}. #{name}"
     end
   else
     puts "The line is currently empty."
