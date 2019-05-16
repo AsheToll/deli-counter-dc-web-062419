fb2a2684649fb7b444da1f78b1077a1096e305f0
Ashley Tolen <tolenashley7@gmail.com>
2019-05-16 15:57:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(deli)
-  while deli > 0
+  while deli.length > 1
     deli.each_with_index do |name, place|
       puts "The line is currently: #{place + 1}. #{name}"
     end
