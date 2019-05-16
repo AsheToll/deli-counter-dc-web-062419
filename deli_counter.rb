db60b49df4e6e80bfab24a8c5d7ec8ff5861fbd6
Ashley Tolen <tolenashley7@gmail.com>
2019-05-16 15:44:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 def line(deli)
   if deli.length > 0
     deli.each_with_index do |place, name|
-      puts "The line is currently: #{place + 1}. #{name}"
+      puts "The line is currently: #{place}. #{name}"
     end
   else
     puts "The line is currently empty."
