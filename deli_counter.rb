ae7a538cd99a6acd914835e074e49c1374db6567
Ashley Tolen <tolenashley7@gmail.com>
2019-05-16 15:40:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def line(deli)
   if deli.length > 0
-    puts "You are #{line.length} in line."
+    puts "You are #{line.length + 1} in line."
   else
     puts "The line is currently empty."
   end
