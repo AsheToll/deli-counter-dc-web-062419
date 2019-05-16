8749b9a09f967b52bbedd527aa9d51d920a8ae54
Ashley Tolen <tolenashley7@gmail.com>
2019-05-16 15:48:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,10 @@
 # Write your code here.
 def line(deli)
   if deli.length > 0
-    deli_line = []
     deli.each_with_index do |name, place|
-      deli_line << "The line is currently: #{place + 1}. #{name}"
+      puts "The line is currently: #{place + 1}. #{name}"
     end
   else
     puts "The line is currently empty."
   end
-  deli_line
 end
